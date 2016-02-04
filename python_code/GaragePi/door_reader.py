9f99bee85a05802a9397777615748c0685ae47ce
Graham <graham.blanshard@gmail.com>
2016-02-04 17:08:42 -0600

    Bug fixes, camera change
    Trying different exposure mode to try and reduce blur. Couple nma
    related crashes.

--- python_code/GaragePi/door_reader.py
+++ python_code/GaragePi/door_reader.py
@@ -8,7 +8,9 @@
 ##
 # Sqlite3 DB created with db/setup.sh
 ##
-# Uses pytz : `sudo pip install pytz`
+# Uses 
+#    pytz :  `sudo pip install pytz`
+#    pynma : (https://github.com/uskr/pynma)
 ##
 
 import sqlite3
@@ -17,6 +19,7 @@ import datetime
 import time
 import picamera
 import pytz
+import pynma
 import os
 
 #########
@@ -24,10 +27,22 @@ import os
 #########
 
 #Variable Setup
+#GPIO Pins
 MAN_DOOR_PIN = 24       #Purple Wire
 GARAGE_DOOR_PIN = 25    #Blue Wire
+
+#Loop Control
 LOOP_DELAY = 1          #sec
 QUIT = False
+
+#NotifyMyAndroid Setup
+NMA_API_KEY = "SuperTopSecret" #Your NMA API key goes here!
+manNotified = False
+garageNotified = False
+NMA_DELAY_SEC = 300     #5 minutes
+NMA_LATE_START = 0      #Midnight	
+NMA_LATE_END = 5        #5AM
+
 manDoorState = None     #None = closed, time = open
 garageDoorState = None  #None = closed, time = open
 savepicdir = './pics/'
@@ -48,6 +63,10 @@ camera.vflip = True
 camera.hflip = True
 camera.resolution = (1366,768)
 camera.quality = 100
+camera.exposure_mode = 'sports'
+
+#PyNMA Setup
+nma = pynma.PyNMA(NMA_API_KEY)
 
 #############
 # FUNCTIONS #
@@ -58,9 +77,17 @@ def now():
 
 def dbEvent(eSource, eName):
     "Store an event in the database"
-    values = (eSource,eName)
-    curs.execute('INSERT INTO events (source,name) VALUES (?,?)', values)
-    conn.commit()
+    try:
+        conn=sqlite3.connect('db/garage.db')
+        curs=conn.cursor()
+
+        values = (eSource,eName)
+        curs.execute('INSERT INTO events (source,name) VALUES (?,?)', values)
+        conn.commit()
+        conn.close()
+    except sqlite3.Error, e:
+        print 'Error on Insert: %s' % e.args[0]
+
     return
 
 def readableDate(start,end):
@@ -74,6 +101,11 @@ def makepicdir(savepath):
         os.stat(savepath)
     except:
         os.mkdir(savepath)
+
+def sendAndroidNotify(event,desc,priority):
+    "Send a message to Android"
+    nma.push("GaragePi", event, desc, "http://172.16.1.76:5000/status",priority) #172.16.1.76 is statically assigned on my network
+    return
 	
 #######
 # RUN #
@@ -86,14 +118,25 @@ while not QUIT:
             if manDoorState is None:
                 dbEvent('ManDoor','Open')
                 manDoorState = now()
+            
+                #Test time, if its too late warn me!
+                if manDoorState.hour >= NMA_LATE_START and manDoorState.hour <= NMA_LATE_END:
+                    sendAndroidNotify('Man Door Opened','Your garage has been opened late!',2)
                 print 'Man Door open'
             else:
-		currTime = datetime.datetime.now()
+		currTime = now()
                 savedir = savepicdir + "%04d%02d%02d/" % (currTime.year, currTime.month, currTime.day)
                 savefilename = savedir + 'garage-' + "%02d%02d%02d.jpg" % (currTime.hour, currTime.minute, currTime.second)
                 makepicdir(savedir)
                 camera.capture(savefilename)
+                
+                #Send Notify if over limit
+                delta = currTime - manDoorState
+                if delta.total_seconds() > NMA_DELAY_SEC and not manNotified:
+                    sendAndroidNotify('Man Door Left Open','Man Door has been left open!',1)
+                    manNotified = True
         else:
+            manNotified = False
             if not manDoorState is None:
                 delta_time = readableDate(manDoorState, now())
                 dbEvent('ManDoor','Close ' + delta_time )
@@ -106,7 +149,20 @@ while not QUIT:
                 dbEvent('GarageDoor','Open')	
                 garageDoorState = now() 
                 print 'Garage Door Open'
+
+                #Test time, if its too late warn me!
+                if garageDoorState.hour >= NMA_LATE_START and garageDoorState.hour <= NMA_LATE_END:
+                    sendAndroidNotify('Garage Door Opened','Your garage has been opened late!',2)
+
+            else:
+                #Send Notify if over limit
+                currTime = now()
+                delta = currTime - garageDoorState
+                if delta.total_seconds() > NMA_DELAY_SEC and not garageNotified:
+                    sendAndroidNotify('Garage Door Left Open','Garage Door has been left open!',1)
+                    garageNotified = True
         else:
+            garageNotified = False
             if not garageDoorState is None:
                 delta_time = readableDate(garageDoorState, now())
                 dbEvent('GarageDoor','Close ' + delta_time )
@@ -118,7 +174,6 @@ while not QUIT:
 	QUIT = true
 
 dbEvent('DoorReader','Stopping')
-conn.close()
 GPIO.cleanup()
 camera.close()
 ############
